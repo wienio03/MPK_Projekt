6c6f71bbfa10b774bfb0ccabd76a27461d758013
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2024-01-06 21:41:20 +0100

    Update CzasPodrozy.sql

--- CzasPodrozy.sql
+++ CzasPodrozy.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION czasPodrozy(adresZajezdni text, adresObecny text)
  AS $function$
     import requests, json
     url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-    parametry = { 'destinations' : adresObecny', 'origins': adresZajezdni, 'units': 'metric', 'key' : 'AIzaSyC1-UIX4rOThD98CTIcHNQ9rwpAqCJfoVg'}
+    parametry = { 'destinations' : adresObecny', 'origins': adresZajezdni, 'units': 'metric', 'key' : 'TODO'}
     wynik = requests.get(url, params=parametry)
     return(wynik.json()['rows'][0]['elements'][0]['duration']['text']) // zwraca zapis tekstowy przewidywanego czasu podrozy
 $function$
